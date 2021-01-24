564491dbd7e2df91124bcb0eb057559b1e9cc9ff
Rachel Moglen <rmoglen@utexas.edu>
2021-01-24 11:47:22 -0600

    initial working pass

--- code/thredup_fullscrape.py
+++ code/thredup_fullscrape.py
@@ -1,178 +1,133 @@
-#!/usr/bin/env python3
-"""
-Author : taniya <https://github.com/tas09009>
-Date   : 2020-10-12
-Purpose: Thredup scrape
-"""
-
 import requests, re, time, random, argparse, os, sys, csv, glob
 from bs4 import BeautifulSoup
 import pandas as pd
 from alive_progress import alive_bar
+import pyperclip
 
 # --------------------------------------------------
 """ Define inputs """
 
-url1 = input("url link: ")
-pages = int(input("# of pages to scrape - 5 minutes/page: "))
-file_name = input("name of file to save as - located in data folder: ")
+#Jeans
+url1 = "https://www.thredup.com/women/jeans/straight-leg-jeans?chars_inseam_in=%5B24%3A26%5D%2C%5B27%3A29%5D%2C%5B30%3A32%5D&chars_jean_wash=light%20wash%2Cmedium%20wash&chars_waist=high%20rise&department_tags=women&search_tags=women-jeans%2Cwomen-jeans-straight-leg%2Cwomen-jeans-skinny%2Cwomen-jeans-boyfriend&state=listed&price=0%2C70"
+# url1= "https://www.thredup.com/women?department_tags=women"
+pages = 2
+file_name = "jeans"
 
 
 # --------------------------------------------------
 def main():
-    '''
-    Input: 
-    - url: searched items
 
-    Output:
-    - hrefs: links of all products on all pages (50 links/page) 
-    - soupified_list: beautiful soup object (parsed html) of each link within hrefs
+	url = url1 + '&page='
+	product_list = []
+	url_front = 'https://www.thredup.com'
 
-    '''
-    url = url1 + '&page='
+	# Everytime range increases, items increase by 50.
+	for page_number in range(1, pages + 1):
+		url_page = ((url) + str(page_number))
 
-    product_list = []
-    # Everytime range increases, items increase by 50.
-    for page_number in range(1, pages + 1):
-        url_page = ((url) + str(page_number))
+		# Parse HTML and pull all href links
+		response = requests.get(url_page)
+		main_page_items = BeautifulSoup(response.text, 'html.parser')
+		grid_products = main_page_items.findAll(
+			'div', {'class': 'grid-item'})
+		for i in grid_products:
+			try:
+				product = i.find('a', {
+					'class': 'WCdF1-WeVI0oEKb0AIa4c'
+					}).get('href')
+				product_list.append(url_front + product)
+			except:
+				print("URL Retrival Error")
 
-        # Parse HTML and pull all href links
-        response = requests.get(url_page)
-        main_page_items = BeautifulSoup(response.text, 'html.parser')
+	image = []
+	description = []
+	materials = []
+	size = []
+	measurements = []
+	price = []
+	brand = []
 
-        hrefs = []
-        product_list = []
-        grid_products = main_page_items.findAll(
-            'div', {'class': 'uiUj-TxKXzmIOHZu6poxM grid-item'})
-        for i in grid_products:
-            product = i.find('a', {
-                'class': '_1di0il_2VkBBwWJz9eDxoJ'
-            }).get('href')
-            product_list.append(product)
+	# with alive_bar(len(product_list)) as bar:
+	progress=0
+	for link in product_list:
+		print(link)
+		print("Progress: ", str(progress),"/",str(len(product_list)))
+		response = requests.get(link)
+		product_page_soupified = BeautifulSoup(response.text,
+											   'html.parser')
 
-        url_front = 'https://www.thredup.com'
-        for a in product_list:
-            hrefs.append(url_front + a)
+		#Product Image Link
+		image_search = product_page_soupified.find(
+			'a', {'class': '_YFsbsE3S64aEw-MiDk2C'}).get('href')
+		image.append(image_search)
 
-            # Lists to store scraped data for each of the 50 items per page
-            category_type = []
-            image = []
-            description = []
-            materials = []
-            size = []
-            measurements = []
-            price = []
-            brand = []
+		#Extract listed details
+		listed_details = product_page_soupified.findAll(
+			 'ul', {'class': 'list-disc'})
 
-        with alive_bar(len(hrefs)) as bar:
-            for link in hrefs:
+		#Product Size and Measurements
+		measurement_size_search = listed_details[1]
+		measurement_size_search = str(measurement_size_search).split("<button")[0:2]
 
-                response = requests.get(link)
-                product_page_soupified = BeautifulSoup(response.text,
-                                                       'html.parser')
+		size.append(measurement_size_search[0].split("<li>")[1].split("</li>")[0])
+		try:
+			measurements.append(measurement_size_search[1].split("<li>")[1].split("</li>")[0])
+		except:
+			#append size if measurements doesn't work
+			measurements.append(measurement_size_search[0].split("<li>")[1].split("</li>")[0])
 
-                product_type = []
-                product_type_search = product_page_soupified.findAll(
-                    'nav', {
-                        'class':
-                        '_3p7XtL0LlyGSi8UgI6EU3j _12-L0I76mLCOu9b4N_SCPU'
-                    })
-                for i in product_type_search:
-                    product = i.find(
-                        'a', {
-                            'class': 'spot-grey-7 JdCj53-vTvU5pLpj6NlFo'
-                        }).getText()
-                    category_type.append(product)
+		#Description
+		description_list=[]
+		description_search = listed_details[0].findAll('li')
+		for i in description_search:
+			description_list.append(i.text)
+		description.append(description_list)
 
-                image_search = product_page_soupified.findAll(
-                    'div', {'class': '_30o7eOhD-KenCXDTlPWxw'})
-                for i in image_search:
-                    product = i.find('a', {
-                        'class': '_17adkz6zswDjAoZ8PhDmPr'
-                    }).get('href')
-                    image.append(product)
+		#Brand
+		brand_search = product_page_soupified.find(
+			'a', {'class': 'ui-link u-text-20'}).get('title')
+		brand.append(brand_search)
 
-                description_mini_list = []
-                description_search = product_page_soupified.findAll(
-                    'ul', {'class': 'aP-V0hf_LsLppqBVnK0PT'})
-                for i in description_search:
-                    description_list_all = i.findAll('li')
-                    for i in description_list_all:
-                        description_text = i.text
-                        description_mini_list.append(description_text)
-                description.append(description_mini_list)
+		#Materials
+		materials_search = product_page_soupified.findAll(
+			'div', {'class': '_2pmDWTgK3W9qnhtkNz7uFZ'})
+		for i in materials_search:
+			if "Materials" in str(i):
+				materials.append(str(i).split("<p>")[1].split("</p>")[0])
 
-                product_materials = []
-                materials_search = product_page_soupified.findAll(
-                    'div', {'class': '_2SuzvN3bcsWRtNcNiSLMGq'})
-                for i in materials_search:
-                    product = i.find('p')
-                    product_materials.append(product)
+		#Price
+		price_search=str(product_page_soupified)
+		price_search=price_search.split("\",\"priceCurrency\":\"USD\"")[1].split("price\":\"")[1]
+		price.append(price_search)
 
-                item = str(product_materials[1])[3:-4]
-                materials.append(item)
+		#For Debugging
+		# print("Size:", size)
+		# print("Measurements: ", measurements)
+		# print("Brand: ", brand)
+		# print("Materials: ", materials)
+		# print("Description: ", description)
+		# print ("Price: ", price)
+		
+		progress += 1
 
-                product_item_measurement_size = []
-                measurement_size_search = product_page_soupified.findAll(
-                    'ul', {'class': 'list-disc'})
-                for i in measurement_size_search:
-                    product = i.findAll('li')
-                    product_item_measurement_size.append(product)
+	basic_scrape = pd.DataFrame({
+				'Link': product_list,
+				'Image_Link': image,
+				# 'Category_Type': category_type,
+				'Description': description,
+				'Materials': materials,
+				'Size': size,
+				'Measurements': measurements,
+				'Price': price,
+				'Brand': brand
+			})
 
-                item = product_item_measurement_size[1]
-                size.append(str(item[0])[4:-13])
-                measurements.append(str(item[1])[4:-5])
+	basic_scrape.to_csv(
+				r'./../data/test_runs/{file_name}.csv'
+				.format(file_name=file_name), index=False, header=True)
 
-                price_search = product_page_soupified.findAll(
-                    'section', {'class': '_36TeFiFjuh5xlahzk4iZeQ'})
-                for i in price_search:
-                    product_price = i.find('span', {
-                        'class': 'RFcwwL7_eda8sdWkyafAr'
-                    }).getText()
-                    price.append(float(product_price[1:]))
-
-                brand_search = product_page_soupified.findAll(
-                    'div', {'class': 'u-flex _20pksgdpcQ2E8r4MYcsBXl'})
-                for i in brand_search:
-                    product = i.find('a', {
-                        'class': '_32zNmjMSfxcoBWGGlzPobp'
-                    }).get('title')
-                    brand.append(product)
-
-                time.sleep(random.randrange(5, 10))
-                bar()
-
-            basic_scrape = pd.DataFrame({
-                'Link': hrefs,
-                'Image_Link': image,
-                'Category_Type': category_type,
-                'Description': description,
-                'Materials': materials,
-                'Size': size,
-                'Measurements': measurements,
-                'Price': price,
-                'Brand': brand
-            })
-
-            basic_scrape.to_csv(
-                r'/home/taniya/Projects/thredup-scraper-api/datasets/test_runs/autumn_clothing/pants{page_number}.csv'
-                .format(page_number=page_number),
-                index=False,
-                header=True)
-
-    # Filter by Fabrics
-    def filter():
-        df = pd.read_csv(
-            f'/home/taniya/Projects/thredup-scraper-api/data/test_runs/merged_{file_name}.csv'
-        )
-        df_materials_banned = ~df.Materials.str.contains(
-            "Polyester|Polyamide|Polyethylene|Acrylic|No Fabric Content")
-        df_materials_banned_removed = df[df_materials_banned]
-        df_materials_banned_removed.to_csv(
-            f'/home/taniya/Projects/thredup-scraper-api/data/test_runs/clean_{file_name}.csv',
-            index=False)
 
 
 # --------------------------------------------------
 if __name__ == '__main__':
-    main()
+	main()
